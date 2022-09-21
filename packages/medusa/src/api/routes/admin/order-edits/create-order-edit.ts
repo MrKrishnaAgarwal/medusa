f7bf86b54260065fc0247ffb8ed9a550fc38795c
Philip Korsholm <philip.korsholm@hotmail.com>
2022-09-21 12:24:03 +0700

    update description in oas of create-order-edit

--- packages/medusa/src/api/routes/admin/order-edits/create-order-edit.ts
+++ packages/medusa/src/api/routes/admin/order-edits/create-order-edit.ts
@@ -7,7 +7,7 @@ import { EntityManager } from "typeorm"
  * @oas [post] /order-edits
  * operationId: "PostOrderEdits"
  * summary: "Create an OrderEdit"
- * description: "Created a OrderEdit."
+ * description: "Create an OrderEdit."
  * x-authenticated: true
  * x-codeSamples:
  *   - lang: JavaScript
